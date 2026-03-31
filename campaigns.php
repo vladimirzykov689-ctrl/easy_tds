673f7114729f859b73d0164cfcc0255c0656ad00
vladimirzykov689-ctrl <vladimirzykov689@gmail.com>
2026-03-31 16:23:54 +0300

    Update campaigns.php

--- campaigns.php
+++ campaigns.php
@@ -151,7 +151,9 @@ $streams = $stmt->fetchAll(PDO::FETCH_ASSOC);
         <span></span>
         <span></span>
     </button>
-    <a href="main.php" class="top-header-title" style="text-decoration:none; color:inherit;">Easy TDS</a>
+    <a href="main.php" style="text-decoration:none; display:flex; align-items:center;">
+    <img src="/img/logo.png" alt="Easy TDS" style="height:40px; width:auto;">
+</a>
 </header>
 
 <!-- ========== MAIN WRAPPER ========== -->
@@ -337,7 +339,9 @@ $streams = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
         </div><!-- /content -->
 
-        <div class="footer">Easy TDS</div>
+        <div class="footer">
+    <img src="/img/logo.png" alt="Easy TDS" style="height:32px; width:auto; vertical-align:middle;">
+</div>
     </div><!-- /page-content -->
 
 </div><!-- /main-wrapper -->
