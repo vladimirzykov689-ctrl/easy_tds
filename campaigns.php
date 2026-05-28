552dd8429b047a8f3fa0b256bd3c464df1c27890
vladimirzykov689-ctrl <vladimirzykov689@gmail.com>
2026-05-28 17:32:06 +0300

    Update campaigns.php

--- campaigns.php
+++ campaigns.php
@@ -202,7 +202,7 @@ $streams = $stmt->fetchAll(PDO::FETCH_ASSOC);
                         </a>
                     </li>
 <li>
-                        <a href="?export=csv">
+                        <a href="?stream_id=<?= $stream_id ?>&export=csv">
                             <span class="nav-icon">
                                 <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8l-6-6zm-1 7V3.5L18.5 9H13zM8 13h8v1.5H8V13zm0 3h8v1.5H8V16zm0-6h3v1.5H8V10z"/>
@@ -212,7 +212,7 @@ $streams = $stmt->fetchAll(PDO::FETCH_ASSOC);
                         </a>
                     </li>
 <li>
-                        <a href="?export=goals_csv">
+                        <a href="?stream_id=<?= $stream_id ?>&export=goals_csv">
                             <span class="nav-icon">
                                 <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm3.88-11.71L10 14.17l-1.88-1.88a.996.996 0 1 0-1.41 1.41l2.59 2.59c.39.39 1.02.39 1.41 0L17.3 9.7a.996.996 0 0 0 0-1.41c-.39-.39-1.03-.39-1.42 0z"/>
